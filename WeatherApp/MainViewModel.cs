62d23674cfde98cf46a106a5661fd05520e25cfd
miloszkryzia1 <milosz.kryzia@gmail.com>
2023-10-28 15:14:35 -0700

    start work on extracting forecast data

--- WeatherApp/MainViewModel.cs
+++ WeatherApp/MainViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using PropertyChanged;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
@@ -18,6 +19,9 @@ namespace WeatherApp
         public float CurrentTemp { get; set; }
         public string CurrentImage { get; set; }
         public string CurrentCity { get; set; }
+        public ObservableCollection<HourlyData> Hourly {  get; set; }
+        //public ObservableCollection<WeatherData> Daily { get; set; }
+
         HttpClient client;
         string baseUrl;
         string key;
@@ -26,19 +30,49 @@ namespace WeatherApp
             client = new HttpClient();
             baseUrl = "https://api.weatherapi.com/v1";
             key = "37685754eee8462993275728232410";
+            Hourly = new ObservableCollection<HourlyData>();
+            //Daily = new ObservableCollection<WeatherData>(); //Separate daily class needed
         }
 
-        public ICommand UpdateWeatherCommand => new Command(async () =>
+        public ICommand UpdateWeatherCommand => new Command(async (searchTerm) =>
         {
-            var url = $"{baseUrl}/current.json?key={key}&q=Paris";
-            var response = await client.GetAsync(url);
-            var content = await response.Content.ReadAsStringAsync();
-            using (var stream = await response.Content.ReadAsStreamAsync())
+            //FIXME - NEED HOURLY FORECAST STARTING TODAY
+            //get Today's data
+            var todayUrl = $"{baseUrl}/forecast.json?key={key}&q={(string)searchTerm}&dt={DateTime.Now.Month}-{DateTime.Now.Day}-{DateTime.Now.Year}";
+            var todayResponse = await client.GetAsync(todayUrl);
+            using var todayStream = await todayResponse.Content.ReadAsStreamAsync();
+            var todayData = await JsonSerializer.DeserializeAsync<ForecastData>(todayStream);
+            
+            //get week data
+            var weekUrl = $"{baseUrl}/forecast.json?key={key}&q={(string)searchTerm}&days=7";
+            var weekResponse = await client.GetAsync(weekUrl);
+            using var weekStream = await weekResponse.Content.ReadAsStreamAsync();
+            var weekData = await JsonSerializer.DeserializeAsync<ForecastData>(weekStream);
+            
+            //assign parameters and lists
+            CurrentCity = todayData.location.name;
+            CurrentTemp = todayData.current.temp_c;
+
+            //Today's hourly
+            var hourlyToday = todayData.forecast.forecastday[0].hour;
+            var hourlyTomorrow = weekData.forecast.forecastday[0].hour;
+            var time = todayData.location.localtime_epoch;
+            var utcTime = DateTimeOffset.FromUnixTimeSeconds(time).DateTime;
+            var timeZoneId = todayData.location.tz_id;
+            var targetTimeZone = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+            var hour = TimeZoneInfo.ConvertTimeFromUtc(utcTime, targetTimeZone).Hour;
+
+            var currentArray = hourlyToday;
+            var i = 0;
+            for (int j = 0; i < 7; j++)
             {
-                var data = await JsonSerializer.DeserializeAsync<WeatherData>(stream);
-                CurrentStatus = data.current.condition.text;
-                CurrentCity = data.location.name;
-                CurrentTemp = data.current.temp_c;
+                if (i > 23)
+                {
+                    i = 0;
+                    currentArray = hourlyTomorrow;
+                }
+                Hourly.Add(new HourlyData(currentArray[i].temp_c, currentArray[i].condition.code));
+                i++;
             }
         });
     }
---
+++ WeatherApp/Models/HourlyData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Tools;
+
+namespace WeatherApp.Models
+{
+    public class HourlyData
+    {
+        public float Temperature { get; set; }
+        public string ImageSource { get; set; }
+
+        public HourlyData(float temp, int conditionCode) 
+        {
+            Temperature = temp;
+            ImageSource = IconSelector.SelectImage(conditionCode);
+        }
+    }
+}
